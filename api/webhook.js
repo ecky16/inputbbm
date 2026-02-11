7ba8d3f4f87419eb48b5fbbb72e130f31bcf27ee
ecky16 <eckyrahmad769@gmail.com>
2026-02-11 19:01:16 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,4 +1,12 @@
 
+
+const { Telegraf } = require('telegraf');
+const axios = require('axios');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyZ9iNgAtS6Vp3ATSRvGZuNFVL_BTHRcLvnGhIbraeulIvR8K57MohgJ4CX3erHO51F/exec"; // GANTI INI
+const CHANNEL_ID = "-1003658290440";
+
 module.exports = async (req, res) => {
   try {
     const { body } = req;
